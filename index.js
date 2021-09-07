47084a936c34c60724421c444abeec075bebca4c
darisantos <89813956+darisantos@users.noreply.github.com>
2021-09-07 17:33:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ functionget imc(){
 var peso=parsefloat(document.get.elementbyid('peso').value);
 var altura=parsefloat(document.get.elementbyid('altura').value);
 var imc=peso/(altura*altura);
-document.getelementbyid("imc".innerhtml=imc}
+document.getelementbyid("imc".innerhtml=imc;}
